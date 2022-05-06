b76caef5e5c0c9f398069f68ab01fb719869e3dc
leemch <lee7mchale@gmail.com>
2022-05-06 12:07:32 -0700

    added navbar with tests

--- src/App.js
+++ src/App.js
@@ -2,16 +2,42 @@ import SignUpPage from './pages/SignUpPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import UserPage from './pages/UserPage';
+import React from 'react';
 
-function App() {
-  return (
-    <div className='container'>
-      {window.location.pathname === '/' && <HomePage />}
-      {window.location.pathname === '/signup' && <SignUpPage />}
-      {window.location.pathname === '/login' && <LoginPage />}
-      {window.location.pathname.startsWith('/user/')  && <UserPage />}
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      path: window.location.pathname
+    }
+  }
+
+  onClickLink = event => {
+    event.preventDefault();
+    const path = event.target.attributes.href.value;
+    window.history.pushState({}, '', path);
+    this.setState({
+      path: path
+    })
+  }
+
+  render() {
+    const {path} = this.state
+    return (
+      <div className='container'>
+        <div>
+          <a href='/'  title='Home' onClick={this.onClickLink}>Hoaxify</a>
+          <a href='/signup' onClick={this.onClickLink}>Sign Up</a>
+          <a href='/login' onClick={this.onClickLink}>Login</a>
+        </div>
+        {path === '/' && <HomePage />}
+        {path === '/signup' && <SignUpPage />}
+        {path === '/login' && <LoginPage />}
+        {path.startsWith('/user/') && <UserPage />}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.spec.js
+++ src/App.spec.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from './App.js';
+import userEvent from "@testing-library/user-event";
 
 
 describe("routing", () => {
@@ -20,7 +21,7 @@ describe("routing", () => {
         setup(path);
         const page = screen.queryByTestId(pageTestId);
         expect(page).toBeInTheDocument();
-    })
+    });
 
     it.each`
     path | pageTestId
@@ -40,6 +41,31 @@ describe("routing", () => {
         setup(path);
         const page = screen.queryByTestId(pageTestId);
         expect(page).not.toBeInTheDocument();
-    })
+    });
 
+
+    it.each`
+    targetPage
+    ${'Home'}
+    ${'Sign Up'}
+    ${'Login'}
+    `("has link to $targetPage on NavBar", ({targetPage}) => {
+        setup("/");
+        const link = screen.getByRole("link", {name: targetPage});
+
+        expect(link).toBeInTheDocument();
+    });
+
+    it.each`
+    initialPath | clickinTo | visiblePage
+    ${'/'}  |  ${'Sign Up'}  |  ${'signup-page'}
+    ${'/signup'}  |  ${'Home'}  |  ${'home-page'}
+    ${'/'}  |  ${'Login'}  |  ${'login-page'}
+    `("displays $visiblePage page after clicking $clickinTo link", ({initialPath, clickinTo, visiblePage}) => {
+        setup(initialPath);
+        const link = screen.getByRole("link", {name: clickinTo});
+        userEvent.click(link);
+        const page = screen.getByTestId(visiblePage);
+        expect(page).toBeInTheDocument();
+    });
 })
